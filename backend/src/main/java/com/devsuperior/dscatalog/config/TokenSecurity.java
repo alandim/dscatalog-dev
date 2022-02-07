c3a9a557cf6d1a72f093793f8e7710d6b49e6e41
Andre Landim <andrelandim_4@hotmail.com>
2022-02-07 19:53:50 -0300

    Implementing OAuth2 authorization server

---
+++ backend/src/main/java/com/devsuperior/dscatalog/config/TokenSecurity.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableWebSecurity
+public class TokenSecurity {
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
