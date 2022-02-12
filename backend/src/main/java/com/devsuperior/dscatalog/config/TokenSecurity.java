c55e12dc2bb92282db275603435ff9582eb3f27f
Andre Landim <andrelandim_4@hotmail.com>
2022-02-12 18:03:47 -0300

    Token additional information

--- backend/src/main/java/com/devsuperior/dscatalog/config/TokenSecurity.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/TokenSecurity.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,10 +11,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableWebSecurity
 public class TokenSecurity {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
